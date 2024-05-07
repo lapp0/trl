a1f5c4024eecead5558f266d3500fcc059bc7097
Andrew Lapp <andrew@rew.la>
2024-05-07 14:41:34 -0500

    try this

--- trl/trainer/policy_trainer_base.py
+++ trl/trainer/policy_trainer_base.py
@@ -266,6 +266,7 @@ class DynamicDataLoader:
 
     def __iter__(self):
         batch_buffer = []
+        import pdb;pdb.set_trace()
         for batch in self.base_dataloader:
             batch_buffer.append(batch)
             if len(batch_buffer) >= self.num_steps:
