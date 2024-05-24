5a204abbd06309f84c985c04366dda032150041b
Andrew Lapp <andrew@github.rew.la>
2024-05-24 01:24:09 -0500

    try fix

--- trl/trainer/ppov2_trainer.py
+++ trl/trainer/ppov2_trainer.py
@@ -282,7 +282,6 @@ class PPOTrainer(PolicyTrainerBase):
 
         ratio = ratio[~padding_mask]
         advantages = advantages[~padding_mask]
-        vf_loss = vf_loss[~padding_mask_p1]
 
         pg_losses = -advantages * ratio
         pg_losses2 = -advantages * torch.clamp(
